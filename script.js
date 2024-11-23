c846b9027e515ca4b9dbbd4814a6aa2fcfd4cce1
Rajat Katiyar <123207263+Rajat-katiyar@users.noreply.github.com>
2024-11-23 22:48:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,28 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	if(arr.length === 1) return 0;
+
+	cost heap = arr.slice().sort((a,b) => a-b);
+
+	let totalCost = 0;
+
+	while(heap.length > 1) {
+		const rope1 = heap.shift();
+		const rope2 = heap.shift();
+
+		const cost = rope1 +rope2;
+		totalCost += cost;
+
+		let insertIndex = heap.findIndex(x => x > cosrt);
+		if(insertIndex === -1) {
+			heap.push(cost);
+		}
+		else {
+			heap.slice(insertIndex, 0, cost);
+		}
+	}
+	return totalCost;
   
 }
 
